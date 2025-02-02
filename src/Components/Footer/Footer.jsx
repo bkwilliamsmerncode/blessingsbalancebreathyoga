7981bdecbb2f8ef41ffdbed269d00f2a4070f3ae
Brian Williams <152632135+bkwilliamsmerncode@users.noreply.github.com>
2025-02-01 19:22:45 -0700

    created to donation portion of the footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -12,24 +12,25 @@ import { FaRegCopyright } from "react-icons/fa6";
 
 
 const Footer = () => {
+
+
+
   return (
     <>
       <div className="footer">
-        <div className="footer-container">
-          <div className="left">
-            <div className="location">
+        <div className="footerContainer">
+          <div className="footerLeft">
+            <div className="location" style={{marginLeft: "10px"}}>
               <FaHome
                 size={20}
                 style={{ color: "black", marginRight: "2rem" }}
               />
-              <div>
+              <div >
                 <p>Phoenix, Arizona</p>
               </div>
             </div>
-
             <div className="email">
-              <p>
-                {" "}
+              <p style={{marginLeft: "10px"}}>
                 <FaMailBulk
                   size={20}
                   style={{ color: "black", marginRight: "2rem" }}
@@ -37,12 +38,15 @@ const Footer = () => {
                 blessingsbalancebreathyoga@gmail.com
               </p>
             </div>
-          </div>
-          <div className="right">
+            <div className="right">
             <p>Site by Brian K. Williams Full-Stack Web Developer</p>
-            <p>
-              <FaRegCopyright />A 501(c)(3) Organization. EIN: 33-1997810 Blessings Balance Breath Yoga 2024
+            <p style={{display: "flex", alignItems: "center"}}>
+              <FaRegCopyright /> A 501(c)(3) Organization. EIN: 33-1997810 Blessings Balance Breath Yoga 2024
             </p>
+            <br />
+            <br />
+            <br />
+            <br />
             <div className="social">
               <a href=" https://www.yogaalliance.org/ " target="_blank">
                 <img src={Logos} alt="logos" className="logosimg" />
@@ -83,8 +87,34 @@ const Footer = () => {
               <a href="https://www.christianyogaassociation.org" rel="noreferrer" target="_blank"><div className="logo2"></div></a>
             </div>
           </div>
+          </div>
+          <div className="footerRight">
+          
+       
+          <div className="donateButtonContainer">
+            <button className="donateButton">Donate Now!</button>
+          </div>
+          <div className="textContainer">
+            <p className="textTitle">
+              Ways to give to Blessings Balance Breath Yoga
+            </p>
+            <p className="textParagraph">
+              Zelle: blessingsbalancebreathyoga@gmail.com
+            </p>
+            <p>Checks mailed to:</p>
+            <p className="textParagraph">Blessings Balance Breath Yoga</p>
+            <p className="textParagraph">3961 East Chandler Blvd Suite 111</p>
+            <p className="textParagraph">PMB #183</p>
+            <p className="textParagraph">Phoenix, AZ 85048</p>
+          </div>
+
+         
+
+       
+     
         </div>
       </div>
+    </div>
     </>
   );
 };
